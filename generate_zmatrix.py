ce6bb659e00d9ad33ad89bad1c4d65127360919e
Vusal Salmanov <salmanov.vh@gmail.com>
2020-02-21 18:30:49 +0300

    makeup "insert_molecule.py", "generate_zmatrix.py"

--- generate_zmatrix.py
+++ generate_zmatrix.py
@@ -2,48 +2,49 @@ import openbabel
 from sys import argv
 import numpy as np
 
-ifname = argv[1] # "1U19.pdb"
-ofname = argv[2] # "1U19-zmatrix.txt"
+ifname = argv[1]  # "1U19.pdb"
+ofname = argv[2]  # "1U19-zmatrix.txt"
 
-izresseq = int(argv[3]) # 296
-zresnm = argv[4] # "REL"
-ozresseq = int(argv[5]) # 297
+izresseq = int(argv[3])  # 296
+zresnm = argv[4]  # "REL"
+ozresseq = int(argv[5])  # 297
 
 startnum = 8000
 zenum = {
     -3: ("N", izresseq - 1, None),
     -2: ("CA", izresseq - 1, None),
-    -1: ("C" , izresseq - 1, None),
-    0: ("N" , izresseq, [-1, -2, -3]),
+    -1: ("C", izresseq - 1, None),
+    0: ("N", izresseq, [-1, -2, -3]),
     1: ("CA", izresseq, [0, -1, -2]),
-    2: ("C" , izresseq, [1, 0, -1]),
-    3: ("O" , izresseq, [2, 1, 0]),
+    2: ("C", izresseq, [1, 0, -1]),
+    3: ("O", izresseq, [2, 1, 0]),
     4: ("CB", izresseq, [1, 2, 3]),
     5: ("CG", izresseq, [4, 1, 2]),
     6: ("CD", izresseq, [5, 4, 1]),
     7: ("CE", izresseq, [6, 5, 4]),
     8: ("NZ", izresseq, [7, 6, 5]),
     9: ("C15", izresseq, [8, 7, 6]),
-    10:("C14", izresseq, [9, 8, 7]),
-    11:("C13", izresseq, [10, 9, 8]),
-    12:("C20", izresseq, [11, 10, 9]),
-    13:("C12", izresseq, [11, 10 ,9]),
-    14:("C11", izresseq, [13, 11, 10]),
-    15:("C10", izresseq, [14, 13, 11]),
-    16:("C9", izresseq, [15, 14, 13]),
-    17:("C19", izresseq, [16, 15, 14]),
-    18:("C8", izresseq, [16, 15, 14]),
-    19:("C7", izresseq, [18, 16, 15]),
-    20:("C6", izresseq, [19, 18, 16]),
-    21:("C5", izresseq, [20, 19, 18]),
-    22:("C18", izresseq, [21, 20, 19]),
-    23:("C4", izresseq, [21, 20, 19]),
-    24:("C3", izresseq, [23, 21, 20]),
-    25:("C2", izresseq, [24, 23, 21]),
-    26:("C1", izresseq, [25, 24, 23]),
-    27:("C17", izresseq, [26, 25, 24]),
-    28:("C16", izresseq, [26, 25, 24]),
+    10: ("C14", izresseq, [9, 8, 7]),
+    11: ("C13", izresseq, [10, 9, 8]),
+    12: ("C20", izresseq, [11, 10, 9]),
+    13: ("C12", izresseq, [11, 10, 9]),
+    14: ("C11", izresseq, [13, 11, 10]),
+    15: ("C10", izresseq, [14, 13, 11]),
+    16: ("C9", izresseq, [15, 14, 13]),
+    17: ("C19", izresseq, [16, 15, 14]),
+    18: ("C8", izresseq, [16, 15, 14]),
+    19: ("C7", izresseq, [18, 16, 15]),
+    20: ("C6", izresseq, [19, 18, 16]),
+    21: ("C5", izresseq, [20, 19, 18]),
+    22: ("C18", izresseq, [21, 20, 19]),
+    23: ("C4", izresseq, [21, 20, 19]),
+    24: ("C3", izresseq, [23, 21, 20]),
+    25: ("C2", izresseq, [24, 23, 21]),
+    26: ("C1", izresseq, [25, 24, 23]),
+    27: ("C17", izresseq, [26, 25, 24]),
+    28: ("C16", izresseq, [26, 25, 24]),
 }
+
 
 def GetAtomByName(idx):
     aname = zenum[idx][0]
@@ -52,59 +53,64 @@ def GetAtomByName(idx):
         name = residue.GetAtomID(obatom).strip()
         if name == aname:
             return obatom
-    raise Exception()
+    assert False, "Atom not found"
+
 
 obConversion = openbabel.OBConversion()
 obConversion.SetInFormat('pdb')
 mol = openbabel.OBMol()
 obConversion.ReadFile(mol, ifname)
 
-outfile = open(ofname, "w")
-outfile.write(f'RESSEQ = {str(ozresseq)}\n')
-outfile.write(f'RESNAME = {zresnm}\n')
-for i in range(len(zenum) - 3):
-    a0 = GetAtomByName(i)
-    a1 = GetAtomByName(zenum[i][2][0])
-    a2 = GetAtomByName(zenum[i][2][1])
-    a3 = GetAtomByName(zenum[i][2][2])
-    
-    # Distance
-    v0 = np.array([a0.x(), a0.y(), a0.z()])
-    v1 = np.array([a1.x(), a1.y(), a1.z()])
-    dist = np.sqrt(np.dot(v0 - v1, v0 - v1))
+with open(ofname, "w") as outfile:
+    outfile.write(f'RESSEQ = {str(ozresseq)}\n')
+    outfile.write(f'RESNAME = {zresnm}\n')
 
-    # ValAngl
-    v2 = np.array([a2.x(), a2.y(), a2.z()])
-    dist01 = dist
-    dist12 = np.sqrt(np.dot(v1 - v2, v1 - v2))
-    dist02 = np.sqrt(np.dot(v0 - v2, v0 - v2))
-    valangl = (dist01 ** 2 + dist12 ** 2 - dist02 ** 2) / (2 * dist01 * dist12)
-    valangl = np.arccos(valangl)
+    for i in range(len(zenum) - 3):
+        a0 = GetAtomByName(i)
+        a1 = GetAtomByName(zenum[i][2][0])
+        a2 = GetAtomByName(zenum[i][2][1])
+        a3 = GetAtomByName(zenum[i][2][2])
 
-    # DihAngl
-    v3 = np.array([a3.x(), a3.y(), a3.z()])
-    b0 = -1.0*(v1 - v0)
-    b1 = v2 - v1
-    b2 = v3 - v2
-    b1 /= np.linalg.norm(b1)
-    v = b0 - np.dot(b0,b1)*b1
-    w = b2 - np.dot(b2,b1)*b1
-    x = np.dot(v,w)
-    y = np.dot(np.cross(b1, v), w)
-    dihangl = np.arctan2(y, x)
+        # Distance
+        v0 = np.array([a0.x(), a0.y(), a0.z()])
+        v1 = np.array([a1.x(), a1.y(), a1.z()])
+        dist = np.sqrt(np.dot(v0 - v1, v0 - v1))
 
-    # Numeration
-    serial = startnum + i
-    con = a1.GetIdx()
-    valcon = a2.GetIdx()
-    dihcon = a3.GetIdx()
-    if i > 0: con = startnum + zenum[i][2][0]
-    if i > 1: valcon = startnum + zenum[i][2][1]
-    if i > 2: dihcon = startnum + zenum[i][2][2]
+        # ValAngl
+        v2 = np.array([a2.x(), a2.y(), a2.z()])
+        dist01 = dist
+        dist12 = np.sqrt(np.dot(v1 - v2, v1 - v2))
+        dist02 = np.sqrt(np.dot(v0 - v2, v0 - v2))
+        valangl = (dist01 ** 2 + dist12 ** 2 - dist02 ** 2) / (2 * dist01 * dist12)
+        valangl = np.arccos(valangl)
 
-    # Write to
-    line = f"{serial}\t{con}\t{dist}\t{valcon}\t{valangl}\t" \
-           f"{dihcon}\t{dihangl}\t{zenum[i][0]}\n"
-    outfile.write(line)
-outfile.close()
+        # DihAngl
+        v3 = np.array([a3.x(), a3.y(), a3.z()])
+        b0 = -1.0 * (v1 - v0)
+        b1 = v2 - v1
+        b2 = v3 - v2
+        b1 /= np.linalg.norm(b1)
+        v = b0 - np.dot(b0, b1) * b1
+        w = b2 - np.dot(b2, b1) * b1
+        x = np.dot(v, w)
+        y = np.dot(np.cross(b1, v), w)
+        dihangl = np.arctan2(y, x)
+
+        # Numeration
+        serial = startnum + i
+        con = a1.GetIdx()
+        valcon = a2.GetIdx()
+        dihcon = a3.GetIdx()
+        if i > 0:
+            con = startnum + zenum[i][2][0]
+        if i > 1:
+            valcon = startnum + zenum[i][2][1]
+        if i > 2:
+            dihcon = startnum + zenum[i][2][2]
+
+        # Write to
+        line = f"{serial}\t{con}\t{dist}\t{valcon}\t{valangl}\t" \
+               f"{dihcon}\t{dihangl}\t{zenum[i][0]}\n"
+        outfile.write(line)
+
 print("OKEY-DOKEY!")
--- insert_molecule.py
+++ insert_molecule.py
@@ -1,20 +1,22 @@
 import numpy as np
 import sys
 import os
-import copy
+
+from copy import copy
+from pathlib import Path
 from scipy.linalg import expm, norm
 from math import sqrt
 
-ifnmol = sys.argv[1] # "Poxr.pdb"
-ifnzmx = sys.argv[2] # "zmatrix.txt"
-ofnmol = sys.argv[3] # "Poxr-result.pdb"
+ifnmol = sys.argv[1]  # "Poxr.pdb"
+ifnzmx = sys.argv[2]  # "zmatrix.txt"
+ofnmol = sys.argv[3]  # "Poxr-result.pdb"
 
-optPath = '/Users/wurthel/Desktop/GitHub/chemistry-scripts/Optimizer/'
-optScript = 'bash relax.sh'
-optMolBef = '1M0L.pdb'
-optMolAft = '1M0L_0.pdb'
-optResid = 'N'
-optSkip = 4
+opt_path = Path("/Users/wurthel/Desktop/GitHub/chemistry-scripts/Optimizer/")
+opt_cmd = "bash relax.sh"
+opt_mol_bef = "1M0L.pdb"
+opt_mol_aft = "1M0L_0.pdb"
+opt_resid = 'N'
+opt_skip = 4
 
 vdwr = {
     'H': 1.10,
@@ -24,22 +26,24 @@ vdwr = {
     'S': 1.80
 }
 
-class Atom():
-    def __init__(self):
-        self.DataType = 'ATOM'  		  # "ATOM"/"HETATM"
-        self.Name = ''          		  # Atom name
-        self.AltLoc = ''        		  # Alternate location indicator. 
-        self.ResName = ''       		  # Residue name
-        self.ChainId = 'A'       		  # Chain identifier
-        self.ResSeq = 0                   # Residue sequence number
-        self.ResCode = ''                 # Code for insertions of residues
-        self.Coordin = np.array([0,0,0])  # (X,Y,Z) orthogonal Å coordinate
-        self.Occup = 0.0        		  # Occupancy
-        self.TempFac = 0.0      		  # Temperature factor
-        self.Element = 'Xx'      		  # Element symbol
-        self.Charge = 0.0         		  # Atom charge
 
-class ZAtom():
+class Atom:
+    def __init__(self):
+        self.DataType = 'ATOM'  # "ATOM"/"HETATM"
+        self.Name = ''  # Atom name
+        self.AltLoc = ''  # Alternate location indicator.
+        self.ResName = ''  # Residue name
+        self.ChainId = 'A'  # Chain identifier
+        self.ResSeq = 0  # Residue sequence number
+        self.ResCode = ''  # Code for insertions of residues
+        self.Coordin = np.array([0, 0, 0])  # (X,Y,Z) orthogonal Å coordinate
+        self.Occup = 0.0  # Occupancy
+        self.TempFac = 0.0  # Temperature factor
+        self.Element = 'Xx'  # Element symbol
+        self.Charge = 0.0  # Atom charge
+
+
+class ZAtom:
     def __init__(self):
         self.Serial = 0
         self.Con = 0
@@ -50,169 +54,175 @@ class ZAtom():
         self.DihAngle = 0.0
         self.Name = 'Xx'
 
+
 def M(axis, theta):
-    return expm(np.cross(np.eye(3), axis/norm(axis)*theta))
+    return expm(np.cross(np.eye(3), axis / norm(axis) * theta))
 
-def valangle(v1, v2):
-    dist01 = np.sqrt(np.dot(v1, v1))
-    dist02 = np.sqrt(np.dot(v2, v2))
-    dist12 = np.sqrt(np.dot(v2 - v1, v2 - v1))
-    angl = (dist01 ** 2 + dist02 ** 2 - dist12 ** 2) / (2 * dist01 * dist02)
-    return np.arccos(angl)
 
-def readPdb(filename):
-    file = open(filename, 'r')
-    Molecule = dict()
-    for line in file.readlines():
-        DataType = line[0:6].strip()
-        if not DataType in ['ATOM', 'HETATM']:
-            continue
-        atom = Atom()
-        atom.DataType = line[0:6].strip()
-        num = int(line[6:11])
-        atom.Name = line[12:16].strip()
-        atom.AltLoc = line[16].strip()
-        atom.ResName = line[17:20].strip()
-        atom.ChainId = line[21].strip()
-        atom.ResSeq = int(line[22:26])
-        atom.ResCode = line[26].strip()
-        x, y, z = list(map(float, [line[30:38], line[38:46], line[46:54]]))
-        atom.Coordin = np.array([x,y,z])
-        atom.Occup = 0.0 # float(line[54:60])
-        atom.Tempfac = 0.0 # float(line[60:66])
-        atom.Element = atom.Name[0] # line[76:78].strip()
-        Molecule[num] = atom
-    file.close()
-    return Molecule
+def val_angle(vec1, vec2):
+    dist01 = np.linalg.norm(vec1)
+    dist02 = np.linalg.norm(vec2)
+    dist12 = np.linalg.norm(vec1 - vec2)
+    angle = (dist01 ** 2 + dist02 ** 2 - dist12 ** 2) / (2 * dist01 * dist02)
+    return np.arccos(angle)
 
-def writePdb(molecule, filename):
-     f = open(filename, "w")
-     for (idx, atom) in molecule.items():
-         line = '{a0:<6}{a1:>5}{s}{a2:>4}{a3:>1}{a4:>3}{s}{a5:>1}' \
-                '{a6:>4}{a7:<1}{s:>3}{a8[0]:>8.3f}{a8[1]:>8.3f}{a8[2]:>8.3f}'\
-                '{a9:>6.2f}{a10:>6.2f}{s:>11}{a11:<2}\n'.format(
-             a0 = atom.DataType, a1 = idx, a2 = atom.Name, a3 = atom.AltLoc,
-             a4 = atom.ResName, a5 = atom.ChainId, a6 = atom.ResSeq, 
-             a7 = atom.ResCode, a8 = atom.Coordin, a9 = atom.Occup, 
-             a10 = atom.TempFac, a11 = atom.Element, s = ' '
-             )
-         f.write(line)
-     f.close()
 
-def readZMatrix(filename):
-    zMatrix = list()
-    zFile = open(ifnzmx, 'r')
-    zResseq = int(zFile.readline().split()[2])
-    zResname = zFile.readline().split()[2]
-    for line in zFile.readlines():
-        words = line.split()
-        zAtom = ZAtom()
-        zAtom.Serial = int(words[0])
-        zAtom.Con = int(words[1])
-        zAtom.Dist = float(words[2])
-        zAtom.ValCon = int(words[3])
-        zAtom.ValAngle = float(words[4])
-        zAtom.DihCon = int(words[5])
-        zAtom.DihAngle = float(words[6])
-        zAtom.Name = words[7].strip()
-        zMatrix.append(zAtom)
-    zFile.close()
+def read_pdb(filename):
+    with open(filename, 'r') as file:
+        molecule = dict()
+        for line in file.readlines():
+            data_type = line[0:6].strip()
+            if data_type not in ['ATOM', 'HETATM']:
+                continue
+            atom = Atom()
+            atom.DataType = line[0:6].strip()
+            num = int(line[6:11])
+            atom.Name = line[12:16].strip()
+            atom.AltLoc = line[16].strip()
+            atom.ResName = line[17:20].strip()
+            atom.ChainId = line[21].strip()
+            atom.ResSeq = int(line[22:26])
+            atom.ResCode = line[26].strip()
+            atom.Coordin = np.array(list(map(float, [line[30:38], line[38:46], line[46:54]])))
+            atom.Occup = 0.0  # float(line[54:60])
+            atom.Tempfac = 0.0  # float(line[60:66])
+            atom.Element = atom.Name[0]  # line[76:78].strip()
 
-    return zResseq, zResname, zMatrix
+            molecule[num] = atom
+
+    return molecule
+
+
+def write_pdb(molecule, filename):
+    f = open(filename, "w")
+    for (idx, atom) in molecule.items():
+        line = '{a0:<6}{a1:>5}{s}{a2:>4}{a3:>1}{a4:>3}{s}{a5:>1}' \
+               '{a6:>4}{a7:<1}{s:>3}{a8[0]:>8.3f}{a8[1]:>8.3f}{a8[2]:>8.3f}' \
+               '{a9:>6.2f}{a10:>6.2f}{s:>11}{a11:<2}\n'.format(
+            a0=atom.DataType, a1=idx, a2=atom.Name, a3=atom.AltLoc,
+            a4=atom.ResName, a5=atom.ChainId, a6=atom.ResSeq,
+            a7=atom.ResCode, a8=atom.Coordin, a9=atom.Occup,
+            a10=atom.TempFac, a11=atom.Element, s=' '
+        )
+        f.write(line)
+    f.close()
+
+
+def read_zmatrix(filename):
+    z_matrix = list()
+    with open(ifnzmx, 'r') as file:
+        resseq = int(file.readline().split()[2])
+        resname = file.readline().split()[2]
+        for line in file.readlines():
+            words = line.split()
+            atom = ZAtom()
+            atom.Serial = int(words[0])
+            atom.Con = int(words[1])
+            atom.Dist = float(words[2])
+            atom.ValCon = int(words[3])
+            atom.ValAngle = float(words[4])
+            atom.DihCon = int(words[5])
+            atom.DihAngle = float(words[6])
+            atom.Name = words[7].strip()
+
+            z_matrix.append(atom)
+
+    return resseq, resname, z_matrix
+
 
 # Remove optFiles
-if os.path.exists(optPath + '/' + optMolBef):
-    os.remove(optPath  + '/' + optMolBef)
-if os.path.exists(optPath  + '/' + optMolAft):
-    os.remove(optPath  + '/' + optMolAft)
-if os.path.exists(optPath + '/' + optResid):
-    os.remove(optPath + optResid)
+if os.path.isfile(opt_path / opt_mol_bef):
+    os.remove(opt_path / opt_mol_bef)
+if os.path.isfile(opt_path / opt_mol_aft):
+    os.remove(opt_path / opt_mol_aft)
+if os.path.isfile(opt_path / opt_resid):
+    os.remove(opt_path / opt_resid)
 
-Molecule = readPdb(ifnmol)
-zResseq, zResname, zMatrix = readZMatrix(ifnzmx)
+molecule = read_pdb(ifnmol)
+z_resseq, z_resname, z_matrix = read_zmatrix(ifnzmx)
 
-for N in range(len(zMatrix)):
-    zAtom = zMatrix[N]
+for N in range(len(z_matrix)):
+    z_atom = z_matrix[N]
 
     atom = Atom()
-    atom.ResSeq = zResseq
-    atom.ResName = zResname
-    atom.Name = zAtom.Name
-    atom.Element = zAtom.Name[0]
+    atom.ResSeq = z_resseq
+    atom.ResName = z_resname
+    atom.Name = z_atom.Name
+    atom.Element = z_atom.Name[0]
 
-    atom1 = Molecule[zAtom.Con]
-    atom2 = Molecule[zAtom.ValCon]
-    atom3 = Molecule[zAtom.DihCon]
+    atom1 = molecule[z_atom.Con]
+    atom2 = molecule[z_atom.ValCon]
+    atom3 = molecule[z_atom.DihCon]
 
     oX = np.array([1, 0, 0])
     oY = np.array([0, 1, 0])
     oZ = np.array([0, 0, 1])
-    v1 = copy.copy(atom1.Coordin)
-    v2 = copy.copy(atom2.Coordin)
-    v3 = copy.copy(atom3.Coordin)
+    v1 = copy(atom1.Coordin)
+    v2 = copy(atom2.Coordin)
+    v3 = copy(atom3.Coordin)
 
     v3 -= v1
     v2 -= v1
 
-    angl1 = valangle(np.array([v2[0], v2[1], 0]), oX)
-    if v2[1] > 0: 
+    angl1 = val_angle(np.array([v2[0], v2[1], 0]), oX)
+    if v2[1] > 0:
         angl1 *= -1
     v2 = np.dot(M(oZ, angl1), v2)
     v3 = np.dot(M(oZ, angl1), v3)
 
-    angl2 = valangle(np.array([v2[0], v2[1], v2[2]]), oX)
+    angl2 = val_angle(np.array([v2[0], v2[1], v2[2]]), oX)
     if v2[2] < 0:
         angl2 *= -1
     v2 = np.dot(M(oY, angl2), v2)
     v3 = np.dot(M(oY, angl2), v3)
 
-    angl3 = valangle(np.array([0, v3[1], v3[2]]), oZ)
+    angl3 = val_angle(np.array([0, v3[1], v3[2]]), oZ)
     if v3[1] < 0:
         angl3 *= -1
     v2 = np.dot(M(oX, angl3), v2)
     v3 = np.dot(M(oX, angl3), v3)
 
-    v = np.array([0,0,0], dtype=float)
-    r01 = zAtom.Dist
+    v = np.array([0, 0, 0], dtype=float)
+    r01 = z_atom.Dist
     r12 = np.sqrt(np.dot(v2, v2))
-    v[0] = np.cos(zAtom.ValAngle) * r01 * r12 / v2[0]
+    v[0] = np.cos(z_atom.ValAngle) * r01 * r12 / v2[0]
     v[2] = np.sqrt(r01 ** 2 - v[0] ** 2)
-    v = np.dot(M(oX, -zAtom.DihAngle), v)
+    v = np.dot(M(oX, -z_atom.DihAngle), v)
 
     v = np.dot(M(oX, -angl3), v)
     v = np.dot(M(oY, -angl2), v)
     v = np.dot(M(oZ, -angl1), v)
     v += v1
 
-    atom.Coordin = copy.copy(v)
+    atom.Coordin = copy(v)
 
     # Optimization
     resSeqList = set()
-    for x in Molecule.values():
-        if x.ResSeq == zResseq:
+    for x in molecule.values():
+        if x.ResSeq == z_resseq:
             continue
-        if x.ResSeq == zResseq - 1:
+        if x.ResSeq == z_resseq - 1:
             continue
-        if x.ResSeq == zResseq + 1:
+        if x.ResSeq == z_resseq + 1:
             continue
         r = atom.Coordin - x.Coordin
         dist = sqrt(np.dot(r, r))
         if dist < (vdwr[atom.Element] + vdwr[x.Element]) * 0.8:
             resSeqList.add(x.ResSeq)
 
-    Molecule[zAtom.Serial] = atom
-    if resSeqList and (optSkip <= 0):
-        oldDir = os.getcwd()
-        os.chdir(optPath)
-        writePdb(Molecule, optMolBef)
-        with open(optResid,'a') as nFile:
-            for n in resSeqList: 
+    molecule[z_atom.Serial] = atom
+    if resSeqList and (opt_skip <= 0):
+        old_dir = os.getcwd()
+        os.chdir(opt_path)
+        write_pdb(molecule, opt_mol_bef)
+        with open(opt_resid, 'a') as nFile:
+            for n in resSeqList:
                 nFile.write(str(n) + '\n')
-        os.system(optScript)
-        Molecule = readPdb(optMolAft)
-        os.chdir(oldDir)
+        os.system(opt_cmd)
+        Molecule = read_pdb(opt_mol_aft)
+        os.chdir(old_dir)
 
-    optSkip -= 1
+    opt_skip -= 1
 
-writePdb(Molecule, ofnmol)
-print ("OKEY-DOKEY!")
+write_pdb(molecule, ofnmol)
+print("OKEY-DOKEY!")
