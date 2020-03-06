1c9633ce1df5c18e370d0007f2080723af7902ce
Vusal Salmanov <salmanov.vh@gmail.com>
2020-03-06 17:37:46 +0300

    Некоторые переделки evolution.py (с учетом новых классов в data)

--- evolution.py
+++ evolution.py
@@ -4,46 +4,44 @@ import time
 import os
 import shutil
 from typing import List, Dict
-from data import Gene, Protein
+from data import Gene, Protein, CHARGED, NON_CHARGED
+# def read_genes(config) -> List[Gene]:
+#     genes = list()
+#     for (position, params) in config.items():
+#         params = params.split(' ')
+#         position = int(position)
+#         variants = params[0]
+#         cros_prob = float(params[1])
+#         mut_prob = float(params[2])
+#         gene = Gene(position=position, variants=variants, cros_prob=cros_prob, mut_prob=mut_prob)
+#         genes.append(gene)
+#     return genes
 
-def read_genes(config) -> List[Gene]:
-    genes = list()
-    for (position, params) in config.items():
-        gene = Gene()
-        params = params.split(' ')
-        gene.position = int(position)
-        gene.variants = params[0]
-        gene.cros_prob = float(params[1])
-        gene.mut_prob  = float(params[2])
-        genes.append(gene)
-    return genes
 
-def generate_population(pop_size: int, genes: List[Gene],
-                       compute_lmb_ouf: str, compute_lmb_inf: str) -> List[Protein]:
+def generate_population(sequence: str, pop_size: int, compute_lmb_ouf: str, compute_lmb_inf: str) -> List[Protein]:
     proteins = list()
     for _ in range(pop_size):
-        protein = Protein(genes)
+        protein = Protein(sequence)
         proteins.append(protein)
 
     if os.path.exists(compute_lmb_ouf) and os.path.exists(compute_lmb_inf):
-        lmb_ouf_file = open(compute_lmb_ouf, 'r')
-        lmb_inf_file = open(compute_lmb_inf, 'r')
-        lmb_ouf_lines = list(filter(None, [line.strip() for line in lmb_ouf_file]))
-        lmb_inf_lines = list(filter(None, [line.strip() for line in lmb_inf_file]))
-        
-        for (protein, l1, l2) in zip(proteins, lmb_ouf_file, lmb_inf_file):
-            protein.update_variance_from_sequence(genes, l1)
-            protein.mlambda = float(l2)
+        lmb_ouf_file = open(compute_lmb_ouf, "r")
+        lmb_inf_file = open(compute_lmb_inf, "r")
+
+        for (protein, seq, val) in zip(proteins, lmb_ouf_file, lmb_inf_file):
+            protein.update_genes_from_sequence(seq)
+            protein.value = float(val)
 
         lmb_ouf_file.close()
         lmb_inf_file.close()
 
-        os.remove(compute_lmb_ouf)
-        os.remove(compute_lmb_inf)
+        # os.remove(compute_lmb_ouf)
+        # os.remove(compute_lmb_inf)
 
     return proteins
 
-def crossover(population: List[Protein], genes: List[Gene], cros_prob: float) -> None:
+
+def crossover(population: List[Protein], cros_prob: float) -> None:
     proteins_for_cross = list()
     for protein in population:
         r = random.random()
@@ -55,35 +53,38 @@ def crossover(population: List[Protein], genes: List[Gene], cros_prob: float) ->
 
     random.shuffle(proteins_for_cross)
 
-    for a, b in zip(proteins_for_cross[0:-1:2], proteins_for_cross[1::2]):
-        for gene in genes:
+    for p1, p2 in zip(proteins_for_cross[0:-1:2], proteins_for_cross[1::2]):
+        for g1, g2 in zip(p1.genes, p2.genes):
+            assert g1.cros_prob == g2.cros_prob and g1.position == g2.position
             r = random.random()
-            if r < gene.cros_prob:
-                pos = gene.position
-                x, y = a.variance[pos], b.variance[pos]
-                a.update_variance(pos, y)
-                b.update_variance(pos, x)
+            if r < g1.cros_prob and g1.type != g2.type:
+                g2.value, g1.value = g1.value, g2.value
+                p1.value, p2.value = None, None
+
 
 def mutation(population: List[Protein], genes: List[Gene], mut_prob: float) -> None:
     for protein in population:
         r = random.random()
         if r < mut_prob:
-            for gene in genes:
+            for gene in protein.genes:
                 r = random.random()
                 if r < gene.mut_prob:
-                    protein.update_variance(gene.position, gene.select_aminoacid())
+                    gene.value = random.choice(NON_CHARGED) if gene.type == "CHARGED" else random.choice(CHARGED)
+                    protein.value = None
+
 
 def evaluate(eval_param: float, n: int) -> float:
     return eval_param * pow(1 - eval_param, n - 1)
 
+
 def selection(population: List[Protein], eval_param: float) -> (List[Protein], List[Protein]):
     for protein in population:
-        if protein.mlambda == None:
+        if protein.value is None:
             print('WARNING: selection is missing. The population contains uncomputed lambda.')
             return [], population
 
     best_proteins, new_population = list(), list()
-    population = sorted(population, key=lambda protein: protein.mlambda, reverse=True)
+    population = sorted(population, key=lambda x: x.value, reverse=True)
     for _ in range(3):
         protein = population.pop(0)
         best_proteins.append(copy.deepcopy(protein))
@@ -101,21 +102,23 @@ def selection(population: List[Protein], eval_param: float) -> (List[Protein], L
 
     return best_proteins, new_population
 
+
 def compute_lambda(population: List[Protein], pattern_seq: str,
-                  computed_proteins: Dict[str, float],
-                  compute_lmb_ouf: str, compute_lmb_inf: str) -> None:
+                   computed_proteins: Dict[str, float],
+                   compute_lmb_ouf: str, compute_lmb_inf: str) -> None:
     proteins_for_computing = list()
     for protein in population:
-        variance = protein.get_variance()
+        variance = ''.join(protein.variance.values())
         if variance in computed_proteins:
-            protein.mlambda = computed_proteins[variance]
+            value = computed_proteins[variance]
+            protein.update_value(value)
         else:
             proteins_for_computing.append(protein)
 
     if proteins_for_computing:
         with open('.tempfile', 'w') as ouf:
             for protein in proteins_for_computing:
-                seq = protein.get_sequence(pattern_seq)
+                seq = protein.sequence
                 ouf.write(seq + '\n')
         os.rename('.tempfile', compute_lmb_ouf)
 
@@ -129,25 +132,28 @@ def compute_lambda(population: List[Protein], pattern_seq: str,
         os.remove(compute_lmb_ouf)
         os.remove(compute_lmb_inf)
 
+
 def save_computing(population: List[Protein], computed_proteins: Dict[str, float], path: str) -> None:
     with open(path, 'a') as ouf:
         for protein in population:
-            variance = protein.get_variance()
-            lmb = protein.mlambda
-            if variance in computed_proteins or lmb == None:
+            sequence = protein.sequence
+            value = protein.value
+            if sequence in computed_proteins or value is None:
                 continue
-            computed_proteins[variance] = lmb
-            line = f'{variance}\t{lmb}\n'
+            computed_proteins[sequence] = value
+            line = f'{sequence}\t{value}\n'
             ouf.write(line)
+
 
 def read_computed_proteins(path: str) -> Dict[str, float]:
     computed_proteins = dict()
-    with open(path, 'r') as inf:
+    with open(path, "r") as inf:
         for line in inf.readlines():
-            variance, value = line.split()
-            computed_proteins[variance] = float(value)
+            sequence, value = line.split()
+            computed_proteins[sequence] = float(value)
     return computed_proteins
 
+
 def get_best_protein(population: List[Protein]) -> Protein:
-    bestProtein = max(population, key=lambda p: p.mlambda)
-    return bestProtein
+    best_protein = max(population, key=lambda x: x.value)
+    return best_protein
--- run_GA.py
+++ run_GA.py
@@ -5,7 +5,6 @@ from evolution import *
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-genes = read_genes(config['GENES'])
 pattern_sequence = config['TEMPLATE']['Sequence']
 cros_prob = float(config['PARAMS']['CrosProb'])
 mut_prob = float(config['PARAMS']['MutProb'])
@@ -18,33 +17,39 @@ result_file_name = config['COMPUTING']['ResultFileName']
 
 # COMPUTING
 computed_proteins = dict()
+population = generate_population(sequence=pattern_sequence, pop_size=pop_size,
+                                 compute_lmb_ouf=compute_lmb_ouf, compute_lmb_inf=compute_lmb_inf)
+# save_computing(population, computed_proteins, computed_proteins_path)
+# best_variance = None
+# if os.path.exists(computed_proteins_path):
+#     computed_proteins = read_computed_proteins(computed_proteins_path)
+#     if computed_proteins:
+#         best_variance = max(computed_proteins, key=computed_proteins.get)
 
-population = generate_population(pop_size, genes, compute_lmb_ouf, compute_lmb_inf)
-save_computing(population, computed_proteins, computed_proteins_path)
-
-best_variance = None
-if os.path.exists(computed_proteins_path):
-    computed_proteins = read_computed_proteins(computed_proteins_path)
-    if computed_proteins:
-        best_variance = max(computed_proteins, key=computed_proteins.get)
+with open("cavity2.txt", "r") as file:
+    for line in file.readlines():
+        pos = int(line)
+        for protein in population:
+            protein[pos - 1].cros_prob = 0.1
+            protein[pos - 1].mut_prob = 0.2
 
 iteration, step, stop_step = 1, 0, 5
 while step < stop_step:
     best_proteins, population = selection(population, eval_param)
-    crossover(population, genes, cros_prob)
-    mutation(population, genes, mut_prob)
+    crossover(population, cros_prob)
+    mutation(population, mut_prob)
     compute_lambda(population, pattern_sequence, computed_proteins, compute_lmb_ouf, compute_lmb_inf)
     
-    population = sorted(population, key=lambda protein: protein.mlambda, reverse=True)
+    population = sorted(population, key=lambda p: p.value, reverse=True)
     population = best_proteins + population[0:pop_size-len(best_proteins)]
 
     cur_best_protein = get_best_protein(population)
-    if best_variance == None:
-        best_variance = cur_best_protein.get_variance()
-    elif cur_best_protein.mlambda <= computed_proteins[best_variance]:
+    if best_variance is None:
+        best_variance = ''.join(cur_best_protein.variance.values())
+    elif cur_best_protein.value <= computed_proteins[best_variance]:
         step += 1
     else:
-        best_variance = cur_best_protein.get_variance()
+        best_variance = ''.join(cur_best_protein.variance.values())
         step = 0
     save_computing(population, computed_proteins, computed_proteins_path)
     line =  f'Iter: {iteration}. The best result: {best_variance}, {computed_proteins[best_variance]}'
@@ -54,9 +59,9 @@ while step < stop_step:
 
 # WRITING RESULTS
 results = sorted(computed_proteins.items(), key=lambda x: x[1], reverse=True)
-protein = Protein(genes)
+protein = Protein(pattern_sequence, genes)
 with open(result_file_name, 'w') as ouf:
-    for variance, lmb in results:
+    for variance, value in results:
         protein.set_variance(variance)
-        line = f'{lmb}, {variance}\n{protein.get_sequence(pattern_sequence)}\n\n'
+        line = f'{value}, {variance}\n{protein.get_sequence(pattern_sequence)}\n\n'
         ouf.write(line)
